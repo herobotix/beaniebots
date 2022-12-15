3431fa5046c06bc47d2f077163dcfda879cfc975
Lucas-183119 <117235895+Lucas-183119@users.noreply.github.com>
2022-12-15 13:21:04 -0700

    Updated code to fit w/ index.html's latest update

--- js/pages.js
+++ js/pages.js
@@ -8,32 +8,8 @@ ready(function (){
 		pageBody.textContent = "";
 		pageBody.appendChild(page);
 	}
-	pages.volunteer = function() { /*volunteer template*/
-		page = document.getElementsByClassName("volunteer template")[0].cloneNode(true);
-		page.classList.remove("template");
-		pageBody.textContent = "";
-		pageBody.appendChild(page);
-	}
-	pages.about = function() { /*about template*/
-		page = document.getElementsByClassName("about template")[0].cloneNode(true);
-		page.classList.remove("template");
-		pageBody.textContent = "";
-		pageBody.appendChild(page);
-	}
-	pages.videos = function() { /*videos template*/
-		page = document.getElementsByClassName("videos template")[0].cloneNode(true);
-		page.classList.remove("template");
-		pageBody.textContent = "";
-		pageBody.appendChild(page);
-	}
-	pages.teams = function() { /*teams template*/
-		page = document.getElementsByClassName("teams template")[0].cloneNode(true);
-		page.classList.remove("template");
-		pageBody.textContent = "";
-		pageBody.appendChild(page);
-	}
-	pages.donations = function() { /*donations template*/
-		page = document.getElementsByClassName("donations template")[0].cloneNode(true);
+	pages.volunteer = function() { /*??? template*/
+		page = document.getElementsByClassName("??? template")[0].cloneNode(true);
 		page.classList.remove("template");
 		pageBody.textContent = "";
 		pageBody.appendChild(page);
@@ -46,11 +22,8 @@ ready(function (){
 	document.getElementById("drkmdbtn").addEventListener("click", drkmdtoggle );
 		
 	function onRedir () {
-		if (location.hash === "#volunteer") pages.volunteer();
-		else if (location.hash === "#about") pages.about();
-		else if (location.hash === "#videos") pages.videos();
-		else if (location.hash === "#teams") pages.teams();
-		else if (location.hash === "#donations") pages.donations();
+		if (location.hash === "#???") pages.???();
+		/*else if (location.hash === "#<unused>") pages.about();*/
 		else pages.homepage();
 	}
 	
